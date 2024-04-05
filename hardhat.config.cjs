54d043ca747ed90c3bf522ba3bfda86f80223288
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-04-05 16:52:28 +0530

    add: hardhar-gas-reporter

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,10 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 require("./tasks/block-number.cjs");
+require("hardhat-gas-reporter");
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia-example";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "p4tk3y";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "3th4p1k3y";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "c01n";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -25,5 +28,13 @@ module.exports = {
     solidity: "0.8.24",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY, // comment for no api call
+        // L1: "polygon", // Default: ethereum
     },
 };
